b4deae511d20f10addccc38dc3a86ba078d7becb
sngtmth <sngtmth@gmail.com>
2021-10-10 16:32:57 +0530

    Added: Append the elements in the list

--- src/main/java/com/day14mavenproject/MyLinkedList.java
+++ src/main/java/com/day14mavenproject/MyLinkedList.java
@@ -22,6 +22,18 @@ public class MyLinkedList {
 		}
 	}
 
+	public void append(INode myNode) {
+		if (this.head == null) {
+			this.head = myNode;
+		}
+		if (this.tail == null) {
+			this.tail = myNode;
+		} else {
+			this.tail.setNext(myNode);
+			this.tail = myNode;
+		}
+	}
+
 	public void printMyNodes() {
 		StringBuilder myNodes = new StringBuilder("My Nodes : ");
 		INode tempNode = head;
