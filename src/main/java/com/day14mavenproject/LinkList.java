aebdc1ffecc835ac148a6ef7fc0291405403a958
sngtmth <sngtmth@gmail.com>
2021-10-10 15:08:43 +0530

    Added:Create simple link list

---
+++ src/main/java/com/day14mavenproject/LinkList.java
@@ -0,0 +1,11 @@
+package com.day14mavenproject;
+
+
+public class LinkList
+{
+    public static void main( String[] args )
+    {
+    	  System.out.println("Welcome to Data Structure using LinkedList ");
+
+    }
+}
