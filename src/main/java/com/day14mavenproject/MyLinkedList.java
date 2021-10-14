5da8d6e4c5e163e2bc53a9076c05af6dccb9e66c
sngtmth <sngtmth@gmail.com>
2021-10-14 14:32:52 +0530

    Added:Search Node by key value

--- src/main/java/com/day14mavenproject/MyLinkedList.java
+++ src/main/java/com/day14mavenproject/MyLinkedList.java
@@ -1,6 +1,6 @@
 package com.day14mavenproject;
 
-public class MyLinkedList {
+public class MyLinkedList<K> {
 	public INode head;
 	public INode tail;
 
@@ -48,18 +48,29 @@ public class MyLinkedList {
 	}
 
 	/* Method is used for deleting the last node */
-	 public void popLast() {
-	        INode tempNode = head;
-	        while (tempNode.getNext() != tail) {
-	            tempNode = tempNode.getNext();
-	        }
-	        tempNode.setNext(null);
-	        tail = tempNode;
-	    }
+	public void popLast() {
+		INode tempNode = head;
+		while (tempNode.getNext() != tail) {
+			tempNode = tempNode.getNext();
+		}
+		tempNode.setNext(null);
+		tail = tempNode;
+	}
 	
-	
-		
-	
+	public INode<K> searchElement(K key)
+    {
+        INode<K> tempNode =head;
+        while (tempNode!=null&&tempNode.getNext()!=null)
+        {
+            if (tempNode.getKey().equals(key))
+            {
+                return tempNode;
+            }
+            tempNode=tempNode.getNext();
+        }
+        return null;
+	}
+
 	public void printMyNodes() {
 		StringBuilder myNodes = new StringBuilder("My Nodes : ");
 		INode tempNode = head;
--- src/test/java/com/day14mavenproject/MyLinkedTest.java
+++ src/test/java/com/day14mavenproject/MyLinkedTest.java
@@ -71,6 +71,22 @@ public class MyLinkedTest {
 
 	@Test
 	public void given3NumbersWhenDeletedLastElementShouldPassLinkedListResult() {
+		MyNode<Integer> myFirstNode =new MyNode<>(56);
+        MyNode<Integer> mySecondNode =new MyNode<>(30);
+        MyNode<Integer> myThirdNode =new MyNode<>(70);
+        MyLinkedList myLinkedList=new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.popLast();
+        myLinkedList.printMyNodes();
+        boolean result=myLinkedList.head.equals(myFirstNode)&&
+                myLinkedList.head.getNext().equals(mySecondNode);
+        Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void given3NumbersWhenSearchElementShouldPassLinkedListResult() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
 		MyNode<Integer> myThirdNode = new MyNode<>(70);
@@ -78,11 +94,11 @@ public class MyLinkedTest {
 		myLinkedList.add(myFirstNode);
 		myLinkedList.append(mySecondNode);
 		myLinkedList.append(myThirdNode);
-		myLinkedList.printMyNodes();
-		myLinkedList.popLast();
-		myLinkedList.printMyNodes();
-		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode);
-		Assert.assertTrue(result);
+		INode tempNode = myLinkedList.searchElement(70);
+		Assert.assertEquals(70,tempNode.getKey());
+		
+		
 	}
+
 
 }
